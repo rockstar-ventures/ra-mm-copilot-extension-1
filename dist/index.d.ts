4b7b8b31dab479311468eff214985df8d516cd27
worka <study.aniket.hingane@gmail.com>
2024-10-24 22:39:11 -0400

    rmove dist

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Plugin from './plugin';
-declare global {
-    interface Window {
-        registerPlugin(id: string, plugin: Plugin): void;
-    }
-}
--- dist/manifest.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: {
-    id: string;
-    version: string;
-};
-export default _default;
